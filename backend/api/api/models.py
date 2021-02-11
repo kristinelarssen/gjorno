ca6b4cf509db5468577bb7f44d4a82731c366027
Iver Gjørvad <iver.gjoervad@ignite.no>
2021-02-11 15:48:06 +0100

    feat(#3): model and view for activities

--- backend/api/api/models.py
+++ backend/api/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Activity(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     created = models.DateTimeField(auto_now_add=True)
     description = models.TextField(default="")
     date = models.DateTimeField()
