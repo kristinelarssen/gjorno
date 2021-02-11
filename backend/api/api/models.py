188b81844db185a241e5313a316916f0216721d5
Iver Gjørvad <iver.gjoervad@ignite.no>
2021-02-11 14:21:50 +0100

    feat(#3): legge til model for aktivitet

--- backend/api/api/models.py
+++ backend/api/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(default="")
+    date = models.DateTimeField()
