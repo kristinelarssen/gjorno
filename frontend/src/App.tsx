59c0628df89c1f41ceb4f9e2d090bed827516ad3
Kristine Larssen <kristinelarssen@dhcp-10-24-1-66.wlan.ntnu.no>
2021-02-11 16:01:31 +0100

    feat(#4): Har laget komponent for aktivitet og navbar.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Activity from './components/Activity';
+//import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <div id = "activities">
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+      </div>
     </div>
   );
 }
+
+const navigation = {
+  brand: { name: "Navbar", to: "/"},
+  links: [
+    //{name: "???", to: "URL"}
+  ]
+}
+
 
 export default App;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import "../../styles/navbar.css"
+
+const Navbar = () => {
+    return (
+        <div id="navbar">
+            <h1 style={{color:"white"}}>GjørNo'</h1>
+        </div>
+    )
+}
+
+
+export default Navbar
