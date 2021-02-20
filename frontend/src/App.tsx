c69060dbbf77675923f70192b6df7d1a385c7567
Jørgen Vartdal Halse <jorgevh@stud.ntnu.no>
2021-02-20 19:13:15 +0100

    feat(#8,#9): fikse App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Activity from './components/Activity';
 //import { render } from '@testing-library/react';
-import { Login } from "./components/Login/index";
+import { Login, Signup } from "./components/Login/index";
+import { isPropertySignature } from 'typescript';
 
 function App() {
+
+  const [state, setState] = useState(false);
+
+  function switchState() {
+    setState(prevState => !prevState)
+  }
+
   return (
     <div className="App">
-      <Login></Login>
+      <div className="switch-state">
+            <button type="button" className="btn" onClick={switchState}>Endre state!</button>
+        </div>
+      <div className="login">
+        <div className="container">
+          {state && <Login />}
+          {!state && <Signup />}
+
+        </div>
+      </div>
     </div>
   )
 }
+
+/**
+const rightRef = useRef()
+const RightSide = () => {
+  return (<div className="Right-side" onClick={switchState}>
+    <div className="text">{(e : any) => e.current}</div>
+  </div>
+
+  )
+}*/
+
+/**
+function RightSide() {
+  const rightRef = useRef()
+
+  return (
+    <div className="right-side" onClick={}></div>
+  )
+} */
 
 /*
 function App() {
