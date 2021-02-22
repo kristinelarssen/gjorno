63853baad978416c748c9ec9b5b733108d9e52af
Jørgen Vartdal Halse <jorgevh@stud.ntnu.no>
2021-02-22 13:19:30 +0100

    fix(#17): slette gammal user-drit

--- backend/api/api/serializers.py
+++ backend/api/api/serializers.py
@@ -1,14 +1,9 @@
 from rest_framework import serializers
-from .models import Activity, User
+from .models import Activity
 
 
 
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        fields = ["id", "title", "created", "description", "date", "owner"]
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User 
-        fields = ["id", "username", "isOrganization", "activitiesLog", "password"]
+        fields = ["id", "title", "created", "description", "date"]
--- backend/api/api/views.py
+++ backend/api/api/views.py
@@ -1,18 +1,11 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from .models import Activity
-from django.contrib.auth.models import User
 from .serializers import ActivitySerializer
-from .serializers import UserSerializer
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
-    permissions = [permissions.IsAuthenticatedOrReadOnly]
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
     permissions = [permissions.IsAuthenticatedOrReadOnly]
 
