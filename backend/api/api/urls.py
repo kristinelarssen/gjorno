99ff757f7de624e2a70c6136f9602d356d8e0f44
elisetelje <teljeelise@gmail.com>
2021-02-23 16:15:28 +0100

    fix(#16): Lagt til kode for admin for å fjerne test bokser

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from django.contrib import admin
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r"activities", views.ActivityViewSet)
 
 # The API URLs are now determined automatically by the router.
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [path("", include(router.urls)), path("admin/", admin.site.urls),]
