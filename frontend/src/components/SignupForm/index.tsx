cd2fc68ca40ad23685c28fb5f76973ea022d3bb9
Iver Gjørvad <iver.gjoervad@ignite.no>
2021-03-26 15:16:02 +0100

    made password validation for sign up

--- frontend/src/components/SignupForm/index.tsx
+++ frontend/src/components/SignupForm/index.tsx
@@ -9,9 +9,10 @@ interface Props {
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     data: IUser
   ) => void;
+  error: string;
 }
 
-const SignupForm: React.FC<Props> = ({ handleSignup }) => {
+const SignupForm: React.FC<Props> = ({ handleSignup, error }) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -19,89 +20,88 @@ const SignupForm: React.FC<Props> = ({ handleSignup }) => {
 
   return (
     <div className="base-container">
-      <form>
-        <div className="header">Registrer deg</div>
-        <div className="content">
-          <div className="image">
-            <img id="login-image" src={loginImage} alt="login" />
-          </div>
-          <div className="form">
-            <div id="float-container">
-              <div id="private" className="float-child">
-                <input
-                  type="radio"
-                  name="radiobutton"
-                  value="private"
-                  onChange={(e) => setIsorganizationSignup(false)}
-                ></input>
-                <label htmlFor="private">Privat person</label>
-              </div>
-              <div id="organization" className="float-child">
-                <input
-                  type="radio"
-                  name="radiobutton"
-                  value="organization"
-                  onChange={(e) => setIsorganizationSignup(true)}
-                ></input>
-                <label htmlFor="organization">Organisasjon</label>
-              </div>
+      <div className="header">Registrer deg</div>
+      <div className="content">
+        <div className="image">
+          <img id="login-image" src={loginImage} alt="login" />
+        </div>
+        <div className="form">
+          <div id="float-container">
+            <div id="private" className="float-child">
+              <input
+                type="radio"
+                name="radiobutton"
+                value="private"
+                onChange={(e) => setIsorganizationSignup(false)}
+              ></input>
+              <label htmlFor="private">Privat person</label>
             </div>
+            <div id="organization" className="float-child">
+              <input
+                type="radio"
+                name="radiobutton"
+                value="organization"
+                onChange={(e) => setIsorganizationSignup(true)}
+              ></input>
+              <label htmlFor="organization">Organisasjon</label>
+            </div>
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="username" className="loginLabel">
-                Brukernavn
-              </label>
-              <input
-                className="loginInput"
-                type="text"
-                placeholder="Brukernavn"
-                value={username}
-                onChange={(event) => setUsername(event.target.value)}
-              ></input>
-            </div>
-            <div className="form-group">
-              <label htmlFor="email" className="loginLabel">
-                E-post
-              </label>
-              <input
-                className="loginInput"
-                type="email"
-                placeholder="E-post"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              ></input>
-            </div>
-            <div className="form-group">
-              <label htmlFor="password" className="loginLabel">
-                Passord
-              </label>
-              <input
-                className="loginInput"
-                type="password"
-                placeholder="Passord"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-              ></input>
-            </div>
+          <div className="form-group">
+            <label htmlFor="username" className="loginLabel">
+              Brukernavn
+            </label>
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Brukernavn"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email" className="loginLabel">
+              E-post
+            </label>
+            <input
+              className="loginInput"
+              type="email"
+              placeholder="E-post"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            ></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="loginLabel">
+              Passord
+            </label>
+            <input
+              className="loginInput"
+              type="password"
+              placeholder="Passord"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
           </div>
         </div>
-        <div className="footer">
-          <button
-            type="submit"
-            className="loginBtn"
-            onClick={(event) => {
-              handleSignup(event, {
-                username: username,
-                email: email,
-                password: password,
-                is_organization: isOrganizationSignup,
-              });
-            }}
-          >
-            Registrer deg
-          </button>
-        </div>
-      </form>
+      </div>
+      <div className="footer">
+        <button
+          className="loginBtn"
+          onClick={(event) => {
+            handleSignup(event, {
+              username: username,
+              email: email,
+              password: password,
+              is_organization: isOrganizationSignup,
+            });
+          }}
+        >
+          Registrer deg
+        </button>
+      </div>
+      <p style={{ color: "red" }}>{error}</p>
+
       <Link to="/login">
         <button id="button-link">Login</button>
       </Link>
